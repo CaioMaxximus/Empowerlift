e78b26429b182b868c88e3d004e3ce1ca489218a
CaioMaxximus <caio.soares@ccc.ufcg.edu.br>
2026-06-18 12:51:43 -0300

    solving cross oring production problem

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from .repositories.quotes_repository import QuotesReposoitory
 from .routers.quotes import router as quotes_router
 from pathlib import Path    
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
 
 ACTUAL_PATH = Path(__file__).resolve()
 
@@ -15,14 +19,17 @@ QuotesReposoitory.init_quotes(QUOTES_PATH)
 api = FastAPI()
 api.include_router(quotes_router)
 
+origins = [
+    "http://localhost:3000",    #React (Create React App)
+    "http://localhost:5173",   
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+]
 
-"/home/caiomaxx/Documentos/projetos/empowerlift/backend/quotes.csv"
-# @api.get('/')
-# async def index():
-#     return {"message" : "Salut!!"}
-
-
-# @api.get('/user/{user_name}')
-# async def get_user(user_name : str):
-#     return {"message": f"vc escolheu o user {user_name}"}
-
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,           
+    allow_credentials=True,          
+    allow_methods=["*"],             
+    allow_headers=["*"],            
+)
