3f3e87bea69fbd569677c9fe552f3f103ab17539
CaioMaxximus <caio.soares@ccc.ufcg.edu.br>
2026-08-02 12:25:59 -0300

    addign build to docker on backend

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -4,6 +4,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     
     api_key: str
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(extra = "ignore")
 
 settings = Settings()
